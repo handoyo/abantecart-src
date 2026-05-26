668939cd3a371baff6085db35795d96f6669decd
abolabo <abolabo@gmail.com>
2026-05-26 17:03:56 +0300

    upgrade: collation fix. ACORN-2011

--- public_html/install/abantecart_database_upgrade.sql
+++ public_html/install/abantecart_database_upgrade.sql
@@ -1,26 +1,38 @@
 DROP TABLE IF EXISTS `ac_collections_to_stores`;
-CREATE TABLE `ac_collections_to_stores` (
-     `collection_id` int(11) NOT NULL,
-     `store_id` int(11) NOT NULL,
-     PRIMARY KEY (`collection_id`,`store_id`)
-) ENGINE=InnoDb DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `ac_collections_to_stores`
+(
+    collection_id INT NOT NULL,
+    store_id      INT NOT NULL,
+    PRIMARY KEY (collection_id, store_id)
+)   ENGINE=InnoDb DEFAULT CHARSET=utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 INSERT INTO `ac_collections_to_stores` (collection_id, store_id)
 SELECT id, store_id
 FROM `ac_collections`;
 
 ALTER TABLE `ac_collections`
-    DROP COLUMN `store_id`,
-    ADD COLUMN `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    modify name varchar(255) null,
-    modify description text null,
-    modify conditions text null
-;
+    MODIFY name VARCHAR(255) NULL,
+    MODIFY description TEXT NULL,
+    MODIFY conditions TEXT NULL,
+    DROP COLUMN store_id,
+    ADD date_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP() NOT NULL ON UPDATE CURRENT_TIMESTAMP();
+
 ALTER TABLE `ac_collection_descriptions`
-    ADD COLUMN `content` longtext NOT NULL COMMENT 'translatable',
-    ADD COLUMN `date_added` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    ADD COLUMN `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    modify title varchar(255) null,
-    modify meta_keywords text null,
-    modify meta_description text null
-;
+    MODIFY title VARCHAR(255) NULL,
+    MODIFY meta_keywords TEXT NULL,
+    MODIFY meta_description TEXT NULL,
+    ADD content LONGTEXT NOT NULL COMMENT 'translatable',
+    ADD date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP() NULL,
+    ADD date_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP() NOT NULL ON UPDATE CURRENT_TIMESTAMP();
+
+
+ALTER TABLE `ac_email_templates`
+    MODIFY text_id VARCHAR(255) NOT NULL,
+    MODIFY headers VARCHAR(255) NOT NULL,
+    MODIFY subject VARCHAR(255) NOT NULL COMMENT 'translatable',
+    MODIFY html_body TEXT NOT NULL COMMENT 'translatable',
+    MODIFY text_body TEXT NOT NULL COMMENT 'translatable',
+    MODIFY allowed_placeholders TEXT NOT NULL;
+
+ALTER TABLE `ac_field_group_to_form`
+    COLLATE = utf8mb4_unicode_ci;
